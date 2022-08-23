5ab532249048c564ae164b648a583a16f80e8cab
DewantaraTP <dewantara.tirta@gmail.com>
2022-08-23 16:41:02 +0700

    fix(010101): fix bug
    fix bug on single food

--- src/pages/FoodPage.js
+++ src/pages/FoodPage.js
@@ -9,10 +9,14 @@ const FoodPage = () => {
     const [food, setFood] = useState(null);
     const apiUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=`;
 
+    const backButton = (e) => {
+        e.preventDefault();
+        window.history.back();
+    }
+
     const fetchData = async () => {
         let data = await fetch(apiUrl + id);
         let result = await data.json();
-
 
         if (result.meals.hasOwnProperty('0')) {
             let meals = result.meals[0];
@@ -30,10 +34,12 @@ const FoodPage = () => {
                 }
                 // console.log(value);
             });
+
             meals.ingredients = ingredients;
             meals.measure = measure;
+            console.log(meals.ingredients)
+
             setFood(meals);
-            console.log(meals)
             // console.log(meals)
         }
     }
@@ -41,46 +47,57 @@ const FoodPage = () => {
     useEffect(() => {
         fetchData();
     }, []);
+    
+    
+    console.log(`>>> food  ingredients<<<`);
+    console.log(food);
 
     return (
         <div>
             {
                 (food) ?
-                    <article id="food-single">
-                        <header>
-                            <div className='img-container' style={{ backgroundImage: `url( ${food.strMealThumb} )` }}></div>
-                            {/* <img src={food.strMealThumb} /> */}
-                        </header>
-                        <h1 id="title">{food.strMeal}</h1>
-                        <p>{food.strInstructions}</p>
+                    <div>
+                        <a href="#" onClick={backButton}>
+                            <h5>{food.strMeal}</h5>
+                        </a>
+                        <article id="food-single">
+                            <header>
+                                <div className='img-container' style={{ backgroundImage: `url( ${food.strMealThumb} )` }}></div>
+                                {/* <img src={food.strMealThumb} /> */}
+                            </header>
+                            <h1 id="title">{food.strMeal}</h1>
+                            <p>{food.strInstructions}</p>
 
-                        <div className='etc'>
-                            <span>Area: {food.strArea}</span>
-                            <span>Category: {food.strCategory}</span>
-                        </div>
+                            <div className='etc'>
+                                <span>Area: {food.strArea}</span>
+                                <span>Category: {food.strCategory}</span>
+                            </div>
 
-                        <h5>Ingredients</h5>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Ingredient Name</th>
-                                    <th scope="col">Measure</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {food.ingredients.forEach( (v, i) => {
-                                    (
-                                        <tr>
-                                            <td>{ (i+1) }</td>
-                                            <td>{food.ingredients[i]}</td>
-                                            <td>{food.measure[i]}</td>
-                                        </tr>
-                                    )
-                                } )}
-                            </tbody>
-                        </table>
-                    </article>
+                            <h5>Ingredients</h5>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Ingredient Name</th>
+                                        <th scope="col">Measure</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                    food.ingredients.map((v, i) => {
+                                        return (
+                                            <tr key={i}>
+                                                <td>{(i + 1)}</td>
+                                                <td>{v}</td>
+                                                <td>{food.measure[i]}</td>
+                                            </tr>
+                                        )
+                                    })
+                                    }
+                                </tbody>
+                            </table>
+                        </article>
+                    </div>
                     : <a href="#" aria-busy="true">🐎 Generating data, please wait…</a>
             }
         </div>
